8cc8afa9d3be43934ace6c2add55b742b9704a5c
Dan Ratcliff <dan_ratcliff@yahoo.com>
2013-07-20 20:32:47 -0500

    Start searching for hidden tile.  Fix row/column vs. y/x mixup bug.

--- TilePuzzle/ViewController.m
+++ TilePuzzle/ViewController.m
@@ -11,8 +11,8 @@
 #import "TilesForRect.h"
 #import "Tile.h"
 
-#define MAX_ROWS 2
-#define MAX_COLUMNS 2
+#define MAX_ROWS 3
+#define MAX_COLUMNS 3
 
 #define BORDER_THICKNESS 5.0
 
@@ -25,6 +25,8 @@
 @property (nonatomic, strong) UIView *tilesContainerView;
 @property (nonatomic, strong) UIView *tilesContainerBorderView;
 @property (nonatomic, strong) TilesForRect *tilesForRect;
+@property (nonatomic, assign) CGFloat tileWidth;
+@property (nonatomic, assign) CGFloat tileHeight;
 
 @end
 
@@ -45,10 +47,14 @@
 }
 
 - (void)viewWillAppear:(BOOL)animated {
+    // Establish the source image.
     self.sourceImage = [UIImage imageNamed:@"globe.jpg"];
     self.sourceImageView = [[UIImageView alloc] initWithImage:self.sourceImage];
     self.sourceImageView.center = CGPointMake(roundf(self.view.bounds.size.width / 2), roundf(self.view.bounds.size.height / 2));
     [self.view addSubview:self.sourceImageView];
+    
+    self.tileWidth = self.sourceImageView.bounds.size.width / MAX_COLUMNS;
+    self.tileHeight = self.sourceImageView.bounds.size.height / MAX_ROWS;
     
     [self initModelForNewGame];
     [self refreshViewForModel];
@@ -83,52 +89,47 @@
     self.tilesContainerView.backgroundColor = [UIColor clearColor];
     [self.view addSubview:self.tilesContainerView];
     
-    CGFloat sourceWidth = self.sourceImageView.bounds.size.width;
-    CGFloat sourceHeight = self.sourceImageView.bounds.size.height;
-    
-    CGFloat tileWidth = sourceWidth / MAX_COLUMNS;
-    CGFloat tileHeight = sourceHeight / MAX_ROWS;
-    
     // Create tiles.
     for (NSUInteger row = 0; row < MAX_ROWS; row ++) {
         for (NSUInteger column = 0; column < MAX_COLUMNS; column++) {
             Tile *tile = [self.tilesForRect getTileForRow:row column:column];
             if (!tile.hidden) {
+                // Tile frame corresponds to location on display.
+                
                 // Border.
                 CGRect borderFrame;
-                borderFrame.size.width = roundf(tileWidth);
-                borderFrame.size.height = roundf(tileHeight);
-                borderFrame.origin.x = roundf(row * tileWidth);
-                borderFrame.origin.y = roundf(column * tileHeight);
+                borderFrame.size.width = roundf(self.tileWidth);
+                borderFrame.size.height = roundf(self.tileHeight);
+                borderFrame.origin.x = roundf(column * self.tileWidth);
+                borderFrame.origin.y = roundf(row * self.tileHeight);
                 UIView *borderView = [[UIView alloc] initWithFrame:borderFrame];
                 borderView.backgroundColor = [UIColor BORDER_COLOR];
-                
                 UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTapFrom:)];
                 [borderView addGestureRecognizer:tapGestureRecognizer];
-                
                 [self.tilesContainerView addSubview:borderView];
                 
-                UIImageView *tileImageView = [[UIImageView alloc] initWithImage:self.sourceImage];
-                
-                // Tile frame corresponds to location on display.
+                // Image within border.
+                UIImageView *tileImageView = [[UIImageView alloc] initWithImage:self.sourceImage];                
                 CGRect imageFrame;
-                imageFrame.size.width = roundf(tileWidth - BORDER_THICKNESS * 2.0);
-                imageFrame.size.height = roundf(tileHeight - BORDER_THICKNESS * 2.0);
+                imageFrame.size.width = roundf(self.tileWidth - BORDER_THICKNESS * 2.0);
+                imageFrame.size.height = roundf(self.tileHeight - BORDER_THICKNESS * 2.0);
                 imageFrame.origin.x = BORDER_THICKNESS;
                 imageFrame.origin.y = BORDER_THICKNESS;
                 tileImageView.frame = imageFrame;
                 
                 // Tile content corresponds to row and column of tiles original location.
                 CGRect cropRect;
-                cropRect.size.width = roundf(tileWidth - BORDER_THICKNESS * 2.0);
-                cropRect.size.height = roundf(tileHeight - BORDER_THICKNESS * 2.0);
-                cropRect.origin.x = roundf(tile.originalRow * tileWidth + BORDER_THICKNESS);
-                cropRect.origin.y = roundf(tile.originalColumn * tileHeight + BORDER_THICKNESS);
+                cropRect.size.width = roundf(self.tileWidth - BORDER_THICKNESS * 2.0);
+                cropRect.size.height = roundf(self.tileHeight - BORDER_THICKNESS * 2.0);
+                cropRect.origin.x = roundf(tile.originalColumn * self.tileWidth + BORDER_THICKNESS);
+                cropRect.origin.y = roundf(tile.originalRow * self.tileHeight + BORDER_THICKNESS);
                 CGImageRef imageRef = CGImageCreateWithImageInRect([self.sourceImage CGImage], cropRect);
                 [tileImageView setImage:[UIImage imageWithCGImage:imageRef]];
                 CGImageRelease(imageRef);
                 
                 [borderView addSubview:tileImageView];
+            } else {
+                NSLog(@"row:%u column:%u ignoring hidden tile:%@", row, column, tile);
             }
         }
     }
@@ -137,8 +138,34 @@
 - (void)handleTapFrom:(UITapGestureRecognizer *)recognizer {
     NSLog(@"recognizer:%@", recognizer);
     
-    // Remove view (for now).
-    [recognizer.view removeFromSuperview];
+    // Deduce row and column from mid-point of view; a little cheezy, but should be safe.
+    NSUInteger row = (recognizer.view.frame.origin.y + recognizer.view.frame.size.height / 2) / self.tileHeight;
+    NSUInteger column = (recognizer.view.frame.origin.x + recognizer.view.frame.size.width / 2) / self.tileWidth;
+    
+    // Search left
+    NSUInteger foundColumn;
+    BOOL found = [self foundHiddenTileSearchingLeftFromRow:row column:column foundColumn:&foundColumn];
+    if (found) {
+        NSLog(@"foundColumn:%u", foundColumn);
+        [recognizer.view removeFromSuperview];
+    }
+    
+
+    
 }
+
+- (BOOL)foundHiddenTileSearchingLeftFromRow:(NSUInteger)row column:(NSUInteger)column foundColumn:(NSUInteger *)foundColumn {
+    BOOL found = NO;
+    for (NSInteger c = column; c >= 0; c--) {
+        Tile *tile = [self.tilesForRect getTileForRow:row column:c];
+        if (tile.hidden) {
+            found = YES;
+            break;
+        }
+    }
+    return found;
+}
+
+
 
 @end
